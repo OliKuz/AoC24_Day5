60398f09c3266f35e4d3bffea05c46c427c7bef4
OliKuz <oleaya.kouzina@gmail.com>
2024-12-05 11:50:19 +0100

    done task 2

--- main.py
+++ main.py
@@ -10,6 +10,8 @@
 # after all of the correct updates are stored, check for their middle value
 # add all of the middle values togather and return the sum
 
+# TASK 2: Fix the incorrect updates and return the sum of the middle values
+
 def main():
 
     input_file = open("input_xs.txt", "r")
@@ -49,14 +51,14 @@ def main():
     # key must be printed at some point before values. (not necessarily immediately before)
 
     valid_updates = []
+    invalid_updates = []
+
     for update in updates:
         pages = update.split(",")
         valid = True
         for page in pages:
             key = page
             values = pages[pages.index(page)+1:]
-            print("Key: ", key)
-            print("Values: ", values)
 
             # check that all of the values after the key are not supposed to come before the key
             for value in values:
@@ -66,7 +68,10 @@ def main():
                         break
         if valid:
             valid_updates.append(update)
+        else:
+            invalid_updates.append(update)
 
+    print()
     print("Valid Updates: ", valid_updates)
 
     # find the middle value of all of the valid updates
@@ -75,13 +80,71 @@ def main():
         pages = update.split(",")
         middle_values.append(pages[len(pages)//2])
 
-    print("Middle Values: ", middle_values)
-
     # add all of the middle values together
     sum = 0         
     for value in middle_values:
         sum += int(value)
-    print("Sum: ", sum)       
+    print("Sum: ", sum)    
+
+    print()
+
+    print("Invalid Updates: ", invalid_updates)   
+
+    # T2 fix the updates
+    fixed_updates = []
+    go = True
+    new_invalid_updates = invalid_updates
+
+    while (go):
+        # continue checking until the updates are no longer being fixed
+        fixed_updates = fix(new_invalid_updates, rules_dict)
+        if fixed_updates == new_invalid_updates:
+            go = False
+        new_invalid_updates = fixed_updates
+
+
+    print()
+    print("Fixed Updates: ", fixed_updates)
+
+    middle_values = []
+    for update in fixed_updates:
+        pages = update.split(",")
+        middle_values.append(pages[len(pages)//2])
+
+    sum = 0
+    for value in middle_values:
+        sum += int(value)
+    
+    print("Fixed Sum: ", sum)
+
+                        
+
+
+
+def fix(invalid_updates, rules_dict):
+
+    fixed_updates = []
+
+    for update in invalid_updates:
+        pages = update.split(",")
+        for page in pages:
+            key = page
+            values = pages[pages.index(page)+1:]
+
+            for value in values:
+                if value in rules_dict:
+                    if key in rules_dict[value]:
+                        print("Fixing: ", key, value)
+                        # try reordering the by swapping the key and value
+                        key_index = pages.index(key)
+                        value_index = pages.index(value)
+                        pages[key_index], pages[value_index] = pages[value_index], pages[key_index]
+                        update = ",".join(pages)
+                        print("Fixed: ", update)
+
+        fixed_updates.append(update)
+
+    return fixed_updates
         
 
 
